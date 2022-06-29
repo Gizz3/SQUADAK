cea4f5d28aed89f87e1d8ad44af00552d636567a
Cero <ewelinatomkiewicz@MacBook-Air-Ewelina.local>
2022-06-30 00:56:39 +0200

    helloaw_wrd

--- src/main/java/pl/squadki/squadki/SquadkiApplication.java
+++ src/main/java/pl/squadki/squadki/SquadkiApplication.java
@@ -16,6 +16,6 @@ public class SquadkiApplication {
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+		return String.format("Hello awful %s!", name);
 	}
 }
