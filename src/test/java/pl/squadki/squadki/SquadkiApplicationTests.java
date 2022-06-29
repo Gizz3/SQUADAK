66833003b2cf1089d99131674ceefb94dbdc039a
Ewelina Tomkiewicz <ewelinatomkiewicz@MacBook-Air-Ewelina.local>
2022-06-30 00:03:55 +0200

    hellowrd

---
+++ src/test/java/pl/squadki/squadki/SquadkiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.squadki.squadki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquadkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
